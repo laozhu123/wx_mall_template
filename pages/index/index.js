6830e78fea91034ae66b31a55902788fa7d7f144
zzy <zzy@192.168.0.102>
2019-10-08 14:20:37 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -237,5 +237,27 @@ Page({
     wx.navigateTo({
       url: "/pages/coupons/index"
     })
-  }
+  },
+  // 以下为搜索框事件
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
 })
